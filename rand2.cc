9fbdb62309d1344cab470365664a5c5b2d51f628
Youngil Cho <youngilcho@nhnent.com>
2019-10-23 16:02:38 +0900

    change random_sample() to sample() with random generator

--- rand2.cc
+++ rand2.cc
@@ -1,20 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <ext/algorithm>
+#include <random>
+#include <algorithm>
+
 
 using namespace std;
-using namespace __gnu_cxx;
+
 
 int main(void)
 {
     vector<int> a;
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 20; ++i) {
+        cout << i << " ";
         a.push_back(i);
     }
+    cout << endl;
 
     vector<int> b;
     b.resize(a.size() / 2);
-    random_sample(a.begin(), a.end(), b.begin(), b.end());
+
+    //random_sample(a.begin(), a.end(), b.begin(), b.end());
+
+    // mt19937 is uniform pseudo random generator
+    sample(a.begin(), a.end(), b.begin(), a.size() / 2, mt19937{ random_device{}() });
+    
     for (uint i = 0; i < b.size(); ++i) {
         cout << b[i] << " ";
     }
